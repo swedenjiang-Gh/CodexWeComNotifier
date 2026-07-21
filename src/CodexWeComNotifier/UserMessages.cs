159abe320176c138e6f9777b36c6e3a38c355fbb
swedenjiang-Gh <sweden.jiang@gmail.com>
2026-07-21 20:22:47 +0800

    Remove obsolete .NET implementation

--- src/CodexWeComNotifier/UserMessages.cs
+++
@@ -1,8 +0,0 @@
-namespace CodexWeComNotifier
-{
-    public static class UserMessages
-    {
-        public const string WebhookSteps = "可以先建两个机器人，然后和机器人拉个群聊，然后点群聊右上方的三个点···，找到消息推送，点“添加”即可提取webhook地址。";
-        public const string HookTrust = "在 Codex 设置的“钩子 / Hooks”里可以找到新的 Hook，设置信任即可。";
-    }
-}
